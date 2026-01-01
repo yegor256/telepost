09d7d49d23e850bf89e0a8b44cadb5cf095ad389
Yegor Bugayenko <yegor256@gmail.com>
2026-01-01 13:11:59 +0300

    year up to 2026

--- test/test_telepost.rb
+++ test/test_telepost.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# SPDX-FileCopyrightText: Copyright (c) 2018-2025 Yegor Bugayenko
+# SPDX-FileCopyrightText: Copyright (c) 2018-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require 'yaml'
@@ -9,7 +9,7 @@ require_relative '../lib/telepost'
 
 # Telepost test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2025 Yegor Bugayenko
+# Copyright:: Copyright (c) 2018-2026 Yegor Bugayenko
 # License:: MIT
 class TelepostTest < Minitest::Test
   def test_fake_posting
